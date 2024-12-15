26b878166b6df22ca8655d515ff935da37029078
Eliud Karuga <eliudkaruga97@gmail.com>
2024-12-15 09:40:47 +0300

    added class validations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  app.setGlobalPrefix('api')
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: 'v1'
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
