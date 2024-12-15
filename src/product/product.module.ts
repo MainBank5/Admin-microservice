305a5cda14160cb4600a75bf4f0ec153e743ad49
Eliud Karuga <eliudkaruga97@gmail.com>
2024-12-16 01:35:56 +0300

    added microservice and rabbitmq

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
+
+  ],
   providers: [ProductService],
   controllers: [ProductController],
 })
